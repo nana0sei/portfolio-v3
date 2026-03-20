7a5c204152cbad4d9f54bb3f07d34ef6ba2ea151
nana0sei <frimpongnanao@gmail.com>
2026-03-20 12:19:26 +0000

    dev card

--- app/components/DevCard.tsx
+++ app/components/DevCard.tsx
@@ -1,9 +1,10 @@
-import { AiFillGithub } from "react-icons/ai";
-import { FiExternalLink } from "react-icons/fi";
 import placeholder from "@/app/assets/placeholder.png";
+import { Card, CardContent } from "@/components/ui/card";
 import { Project } from "@/lib/entities";
+import { SquareArrowOutUpRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { AiFillGithub } from "react-icons/ai";
 
 interface Props {
   project: Project;
@@ -12,36 +13,36 @@ interface Props {
 const DevCard = ({ project }: Props) => {
   return (
     <>
-      <div className="card bg-base-100 shadow-xl">
-        <figure>
-          <Image
-            width={1000}
-            height={1000}
-            src={project.image || placeholder}
-            alt={project.name}
-            className="w-full object-cover"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">
-            <div className="flex gap-1">
-              <p> {project.name}</p>
-              {project.link && (
-                <Link href={project.link} target="_blank">
-                  <FiExternalLink size="25px" />
-                </Link>
-              )}
+      <Card className="shadow-xl">
+        <Image
+          width={1000}
+          height={1000}
+          src={project.image || placeholder}
+          alt={project.name}
+          className="w-full object-cover"
+        />
+        <CardContent className="space-y-1">
+          <div className="flex gap-1 items-center">
+            <p className="font-bold text-lg"> {project.name}</p>
+            {project.link && (
+              <Link
+                href={project.link}
+                target="_blank"
+                className="hover:text-blue-400"
+              >
+                <SquareArrowOutUpRight size={20} />
+              </Link>
+            )}
 
-              {project.git && (
-                <Link href={project.git} target="_blank">
-                  <AiFillGithub size="25px" />
-                </Link>
-              )}
-            </div>
-          </h2>
+            {project.git && (
+              <Link href={project.git} target="_blank">
+                <AiFillGithub size={20} />
+              </Link>
+            )}
+          </div>
           <p className="italic">{project.description}</p>
-        </div>
-      </div>
+        </CardContent>
+      </Card>
     </>
   );
 };
--- lib/data/projects.ts
+++ lib/data/projects.ts
@@ -6,7 +6,7 @@ const projects: Project[] = [
   {
     name: "loangrid",
     description:
-      "a powerful credit scoring engine designed for financial institutions, government programs, and development partners to assess creditworthiness of beneficiaries quickly and accurately. built for scalability and easy integration, the system enables responsible lending, minimizes risk, and promotes financial inclusion for underserved populations.",
+      "a credit scoring engine designed for financial institutions, government programs, and development partners to assess creditworthiness of beneficiaries quickly and accurately. built for scalability and easy integration, the system enables responsible lending, minimizes risk, and promotes financial inclusion for underserved populations.",
     image: loangrid,
     link: "https://logicielghana.com/platorms/platforms/loangrid",
     git: "",
